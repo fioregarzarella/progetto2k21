44a651393cdb8fa731ca1040414742feaabb869f
Fiore Garzarella <s1089186@studenti.univpm.it>
2020-06-30 18:33:32 +0200

    primo commit

---
+++ Progetto/src/main/java/esameProgrammazione/Progetto/ProgettoApplication.java
@@ -0,0 +1,14 @@
+package esameProgrammazione.Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoApplication.class, args);
+	System.out.println("ciao mondo");
+	}
+
+}
---
+++ Progetto/src/test/java/esameProgrammazione/Progetto/ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package esameProgrammazione.Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
